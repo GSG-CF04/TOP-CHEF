faeae9504a0f2e543281161476a86c96c6d60dba
unknown <israaaburayya2012@gmail.com>
2021-12-01 22:25:43 +0200

    fetch Drink categories from api (categoriesDrink page) RELATES#40

---
+++ js/categoryDrink.js
@@ -0,0 +1,42 @@
+let catDrink=document.getElementsByClassName('categories-drink')
+let catDrinkName=[]
+fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+.then(res =>res.json())
+.then(
+    data =>  {
+        let categories=data.drinks
+        categories.forEach(ele => {
+            catDrinkName.push(`${ele.strCategory}`)     
+    })
+    console.log(catDrinkName)
+    localStorage.setItem('catDrinkName',JSON.stringify(catDrinkName))
+ }   
+)
+.catch(err => alert(err))
+
+let catDrinkNameLocal=JSON.parse(localStorage.getItem("catDrinkName"))
+    console.log(catDrinkNameLocal)
+    console.log(catDrinkNameLocal.length)
+for( let i=0;i<catDrinkNameLocal.length;i++) {
+           console.log(catDrinkNameLocal.length)
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${catDrinkNameLocal[i]}`)
+    .then(res =>res.json())
+    .then(
+            dataCat =>{
+            console.log(catDrinkNameLocal)
+            let dataDrink=dataCat.drinks
+                for (let x=0;x<1;x++) {
+                       catDrink[0].innerHTML +=`<a class="drink-cat" href="../type/typeDrink.html">
+                         <div class="category" style="background-image: url(${dataDrink[0].strDrinkThumb})">
+                         <div class="overlay">
+                           <span>${catDrinkNameLocal[i]}</span>
+                         </div>
+                       </div>
+                       </a>`
+                }
+            
+            }
+        ) 
+    .catch(err => alert(err))               
+}
+
