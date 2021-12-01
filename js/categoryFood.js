7ab439a278b866c072b37320027cb6e159cb5e2f
unknown <israaaburayya2012@gmail.com>
2021-12-01 21:39:48 +0200

    addEventListener click to image categories (categoriesFood page) RELATES#39

--- js/categoryFood.js
+++ js/categoryFood.js
@@ -4,8 +4,22 @@ fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
 .then(
     data =>  {
         let categories=data.categories
-        console .log(categories)     
-   
+        categories.forEach(ele => {
+            catMeal[0].innerHTML +=`<a class="a-cat" href="../type/typeFood.html">
+            <div class="category" style="background-image: url(${ele.strCategoryThumb})">
+              <div class="overlay">
+                <span>${ele.strCategory}</span>
+              </div>
+            </div>
+            </a>`     
+    })
+    let link=document.querySelectorAll('.a-cat')
+    for (let i=0 ;i<link.length;i++){
+        link[i].addEventListener('click',(e)=>{
+            const nameCategory=link[i].childNodes[1].childNodes[1].childNodes[1].textContent
+            localStorage.setItem('catFood',nameCategory)
+        })
+    }
  }   
 )
 .catch(err => alert(err))
