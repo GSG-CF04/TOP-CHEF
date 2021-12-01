d5055e4b4c2fb7f3e1394528122cd84e4ea3794a
unknown <israaaburayya2012@gmail.com>
2021-12-01 22:41:01 +0200

    edit empty tag

--- js/typeDrink.js
+++ js/typeDrink.js
@@ -1,7 +1,6 @@
 let head=document.getElementById('name-category')
 let cat=localStorage.getItem('catDrink')
 head.textContent=cat + ' Category' 
-
 let typeDrink=document.getElementsByClassName('cards-drink')
 fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${cat}`)
 .then(res =>res.json())
@@ -13,8 +12,7 @@ fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${cat}`)
             <img src="${drink.strDrinkThumb}" alt="food">
             <p>${drink.strDrink}</p>
             <button class="get-recipe" onclick="getRecipeAndIngradiant(${drink.idDrink})">Get Recipe</button>
-            </div>
-            `   
+            </div>`   
         })
     }
 )
