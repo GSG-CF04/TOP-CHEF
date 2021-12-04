957ea974f936c743c9c99bea56f8635541c85df8
unknown <israaaburayya2012@gmail.com>
2021-12-04 13:07:20 +0200

    add drink categories to local storage to reduce request for api

--- js/categoryDrink.js
+++ js/categoryDrink.js
@@ -1,5 +1,7 @@
 let catDrink=document.getElementsByClassName('categories-drink')
 let catDrinkName=[]
+let catDrinkLocal=[]
+const fetchCategories = () => {
 fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
 .then(res =>res.json())
 .then(
@@ -7,43 +9,56 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
         let categories=data.drinks
         categories.forEach(ele => {
             catDrinkName.push(`${ele.strCategory}`)     
-    })
-    console.log(catDrinkName)
+        })
     localStorage.setItem('catDrinkName',JSON.stringify(catDrinkName))
- }   
+    }   
 )
 .catch(err => alert(err))
 
-let catDrinkNameLocal=JSON.parse(localStorage.getItem("catDrinkName"))
-    console.log(catDrinkNameLocal)
-    console.log(catDrinkNameLocal.length)
+let catDrinkNameLocal=JSON.parse(localStorage.getItem("catDrinkName")) || []
 for( let i=0;i<catDrinkNameLocal.length;i++) {
-           console.log(catDrinkNameLocal.length)
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${catDrinkNameLocal[i]}`)
     .then(res =>res.json())
     .then(
             dataCat =>{
-            console.log(catDrinkNameLocal)
             let dataDrink=dataCat.drinks
                 for (let x=0;x<1;x++) {
-                       catDrink[0].innerHTML +=`<a class="drink-cat" href="../type/typeDrink.html">
-                         <div class="category" style="background-image: url(${dataDrink[0].strDrinkThumb})">
-                         <div class="overlay">
-                           <span>${catDrinkNameLocal[i]}</span>
-                         </div>
-                       </div>
-                       </a>`
+                       addCategories(dataDrink[0].strDrinkThumb,catDrinkNameLocal[i])
+                       addToLocalStorage(dataDrink[0].strDrinkThumb,catDrinkNameLocal[i])
                 }
-                let link=document.querySelectorAll('.drink-cat')
-                for (let i=0 ;i<link.length;i++){
-                    link[i].addEventListener('click',(e)=>{
-                        console.log(link[i].childNodes[1].childNodes[1].childNodes[1])
-                        const nameCategory=link[i].childNodes[1].childNodes[1].childNodes[1].textContent
-                        localStorage.setItem('catDrink',nameCategory)
-                    })
-                }
-
             }
         ) 
     .catch(err => alert(err))               
 }
+}
+
+const addToLocalStorage = function (image, nameCat) {
+    catDrinkLocal.push({ imageSrc: image, name: nameCat });
+    localStorage.setItem("categoriesDrinkLocal", JSON.stringify(catDrinkLocal));
+  };
+window.onload = () => {
+    let getCatLocal =JSON.parse(localStorage.getItem("categoriesDrinkLocal")) || []
+    if (getCatLocal.length === 0) {
+      fetchCategories()
+    }else {
+      getCatLocal.forEach(ele => {
+        addCategories(ele.imageSrc,ele.name) 
+    })
+  }
+  }
+  const addCategories = (imageSource, name) => {
+    catDrink[0].innerHTML +=`<a class="drink-cat" href="../type/typeDrink.html">
+    <div class="category" style="background-image: url(${imageSource})">
+    <div class="overlay">
+      <span>${name}</span>
+    </div>
+  </div>
+  </a>`
+  let link=document.querySelectorAll('.drink-cat')
+  for (let i=0 ;i<link.length;i++){
+      link[i].addEventListener('click',(e)=>{
+          const nameCategory=link[i].childNodes[1].childNodes[1].childNodes[1].textContent
+          localStorage.setItem('catDrink',nameCategory)
+      })
+  }
+  };
