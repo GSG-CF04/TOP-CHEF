946be1b5bfb0139291fd85d04d1dd223fbe2b645
farahalzard <13.farah.6@gmail.com>
2021-12-05 21:02:03 +0200

    get-recipe-button-function Relates #48

--- js/typeDrink.js
+++ js/typeDrink.js
@@ -4,43 +4,52 @@ const searchInp = document.querySelector(".search-control");
 const holder = document.querySelector(".cards-drink");
 let titelCat = document.querySelector(".header-search");
 let catName = localStorage.getItem('catDrink');
+let parent = document.querySelector(".overlay");
 /* End Selectors */
 /* Start Event Listener */
 titelCat.innerHTML= `<h2>${catName} Category</h2>`;
 let final = [];
 searchInp.addEventListener("keyup", (e) => {
     const searchString = e.target.value;
-    let filterd = final.drinks.filter((ch) => {
-        return ch.strDrink.toUpperCase().includes(searchString.toUpperCase());
+    let filterd = final.drinks.filter((drink) => {
+        return drink.strDrink.toUpperCase().includes(searchString.toUpperCase());
     });
     if(e.key ==="Enter"){searchInp.value=""}
-    showMeals(filterd);
+    showDrinks(filterd);
 });
 /* End Event Listener */
 /* Start Function */ 
-const loadCh = async () => {
+const loadDrinks = async () => {
     try {
     let res = await fetch(
         `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${catName}`
     );
     final = await res.json();
         let arr  = final.drinks;
-    showMeals(arr);
+    showDrinks(arr);
     } catch (err) {
     console.log(err);
     }
 };
-loadCh();
-const showMeals = (ch) => {
-    const htmlDiv = ch
-    .map((ch) => {
+loadDrinks();
+const showDrinks = (drink) => {
+    const htmlDiv = drink
+    .map((drink) => {
         return `<div class="card">
-            <img src="${ch.strDrinkThumb}" alt="food">
-            <p>${ch.strDrink}</p>
-            <button class="get-recipe">Get Recipe</button>
+            <img src="${drink.strDrinkThumb}" alt="drink">
+            <p>${drink.strDrink}</p>
+            <button class="get-recipe" onclick="getDrinkRecipe(${drink.idDrink})">Get Recipe</button>
         </div>`;
     })
     .join("");
     holder.innerHTML = htmlDiv;
 };
 /* End Function */ 
+// getting Drinks's data
+function getDrinkRecipe(e) {
+    // show a popup after clicking get recipe btn
+    parent.style.display = "block";
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${e}`)
+    .then((response) => response.json())
+    .then((data) => drinkRecipeModal(data.drinks));
+}
