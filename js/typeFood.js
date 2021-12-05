99722e61864d04fd8c6481f6b526e2f8f2ad536b
farahalzard <13.farah.6@gmail.com>
2021-12-05 20:13:40 +0200

    create the content for food popup

--- js/typeFood.js
+++ js/typeFood.js
@@ -5,6 +5,8 @@ const holder = document.querySelector(".cards-food");
 let titelCat = document.querySelector(".header-search");
 let catName = localStorage.getItem('catFood');
 let parent = document.querySelector(".overlay");
+let container = document.createElement("div");
+let recipeBar = document.querySelector(".bar");
 /* End Selectors */
 /* Start Event Listener */ 
 titelCat.innerHTML= `<h2>${catName} Category</h2>`;
@@ -52,4 +54,44 @@ function getMealRecipe(e) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${e}`)
     .then((response) => response.json())
     .then((data) => mealRecipeModal(data.meals));
+}
+// create the popup content for meal recipe
+function mealRecipeModal(meal) {
+    container.setAttribute("class", "pop-up");
+    parent.appendChild(container);
+    container.innerHTML = `
+    <div id="head-container"><i class="fas fa-times-circle close" onclick="closePopupWindow()"></i>
+        <h3 class = "recipe-title">${meal[0].strMeal}</h3>
+        <div class="bar">
+        <button id="ing" class=" ingr-btn-style" onclick="showIngredients()">Ingredients</button>
+        <button id="rec" class=" recpie-btn" onclick="showRecipe()">Recpie</button>
+    </div>
+    </div>
+    <div class="ing-with-measures">
+        <div class="measures"><p class="measure-P"></p></div>
+        <div class="ing-text"><p class="ing-p"></p></div>
+    </div>
+    <div class="recpie-text">${meal[0].strInstructions}</div>`;
+let measureP = document.querySelector(".measure-P");
+for (const key of Object.keys(meal[0])) {
+    for (let i = 1; i <= 20; i++) {
+    if (
+        key === `strMeasure${i}` &&
+        meal[0][key] != null &&
+        meal[0][key] != ""
+    )
+        measureP.innerHTML += `${meal[0][key]}<br>`;
+    }
+}
+let ingP = document.querySelector(".ing-p");
+for (const key of Object.keys(meal[0])) {
+    for (let i = 1; i <= 20; i++) {
+    if (
+        key === `strIngredient${i}` &&
+        meal[0][key] != null &&
+        meal[0][key] != ""
+    )
+        ingP.innerHTML += `${meal[0][key]}<br>`;
+    }
+}
 }
